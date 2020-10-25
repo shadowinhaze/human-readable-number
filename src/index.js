d7ac347b41d1490b0bd8d2fe0bec9512bdf09c13
shadowinhaze <shadowinhaze@gmail.com>
2020-10-25 19:32:43 +0300

    fix: bug withs spaces

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-module.exports = function toReadable (number) {
-    
-    const ones = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen ','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    const tys = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-    
+module.exports = function toReadable(number) { 
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen','fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tys = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number === 0) return 'zero';
     n = ('000' + number).substr(-3).match(/^(\d{1})(\d{2})$/);
     if (!n) return;
 	let str = '';
     str += (n[1] != 0) ? (ones[Number(n[1])] || tys[n[1][0]] + ' ' + ones[n[1][1]]) + ' hundred ' : '';
     str += (n[2] != 0) ? (ones[Number(n[2])] || tys[n[2][0]] + ' ' + ones[n[2][1]]) : '';
-    return str;
-
+    return str.replace(/\s?$/,'');
 }
+
+// console.log(toReadable(980) + '1');
