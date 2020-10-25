5365753c53c29f0f11470aa6e2bc3d5e380439ce
John Pol <shadowinhaze@gmail.com>
2020-10-25 18:44:33 +0300

    init: start and solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    
+    const ones = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen ','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const tys = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    
+    n = ('000' + number).substr(-3).match(/^(\d{1})(\d{2})$/);
+    if (!n) return;
+	let str = '';
+    str += (n[1] != 0) ? (ones[Number(n[1])] || tys[n[1][0]] + ' ' + ones[n[1][1]]) + ' hundred ' : '';
+    str += (n[2] != 0) ? (ones[Number(n[2])] || tys[n[2][0]] + ' ' + ones[n[2][1]]) : '';
+    return str;
+
 }
